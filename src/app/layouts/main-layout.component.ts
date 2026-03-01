9374e9fb2f85e415f801122b03305192340a5fc5
shihab.v <devteam@muthoottu.com>
2026-03-01 12:30:43 +0530

    changes

--- src/app/layouts/main-layout.component.ts
+++ src/app/layouts/main-layout.component.ts
@@ -1,15 +1,13 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
-import { SidebarComponent } from '../components/sidebar/sidebar.component';
 
 @Component({
-    selector: 'app-main-layout',
-    standalone: true,
-    imports: [RouterOutlet, HeaderComponent, SidebarComponent],
-    template: `
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
     <div class="app-container">
-      <app-sidebar></app-sidebar>
       <div class="main-content">
         <app-header></app-header>
         <main>
@@ -18,7 +16,7 @@ import { SidebarComponent } from '../components/sidebar/sidebar.component';
       </div>
     </div>
   `,
-    styles: [`
+  styles: [`
     .app-container {
       display: flex;
       min-height: 100vh;
@@ -26,18 +24,11 @@ import { SidebarComponent } from '../components/sidebar/sidebar.component';
     }
     .main-content {
       flex: 1;
-      margin-left: var(--sidebar-width);
       display: flex;
       flex-direction: column;
-      width: calc(100% - var(--sidebar-width));
-    }
-    @media (max-width: 768px) {
-      .main-content {
-        margin-left: 0;
-        width: 100%;
-      }
+      width: 100%;
     }
   `],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainLayoutComponent { }

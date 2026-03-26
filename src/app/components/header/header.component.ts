0d8998268734c39299dcf625a533942457356f2d
shihab.v <devteam@muthoottu.com>
2026-03-26 12:21:19 +0530

    Update header and home styling for deployment

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -26,27 +26,46 @@ export class HeaderComponent {
         this.isScrolled.set(window.pageYOffset > 20);
     }
 
+    @HostListener('window:resize', [])
+    onWindowResize() {
+        if (typeof window !== 'undefined' && window.innerWidth > 1024 && this.isMenuOpen()) {
+            this.closeMobileMenu();
+        }
+    }
+
     @HostListener('document:click', ['$event'])
     clickout(event: Event) {
         if (!this.elementRef.nativeElement.contains(event.target)) {
             this.openMenu.set('');
             this.countryMenuOpen.set(false);
+            this.isMenuOpen.set(false);
         }
     }
 
     toggleMenu() {
-        this.isMenuOpen.update(v => !v);
+        const next = !this.isMenuOpen();
+        this.isMenuOpen.set(next);
+
+        if (!next) {
+            this.openMenu.set('');
+        }
     }
 
     toggleMega(menu: string) {
-        this.openMenu.update(v => v === menu ? '' : menu);
+        this.openMenu.update((value) => value === menu ? '' : menu);
     }
 
     toggleCountryMenu() {
-        this.countryMenuOpen.update(v => !v);
+        this.countryMenuOpen.update((value) => !value);
     }
 
     toggleTheme() {
         this.themeService.toggleTheme();
+    }
+
+    closeMobileMenu() {
+        this.isMenuOpen.set(false);
+        this.openMenu.set('');
+        this.countryMenuOpen.set(false);
     }
 }
